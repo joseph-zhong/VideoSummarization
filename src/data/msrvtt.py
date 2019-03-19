2fa70517ffbd49eaf0c76109018ef4e4e1e9f90c
Ryan Rowe <ryanf.rowe@gmail.com>
2019-03-18 17:13:02 -0700

    data/msrvtt: use new caption cache files

--- src/data/msrvtt.py
+++ src/data/msrvtt.py
@@ -1,10 +1,14 @@
-import json
-import os
+"""
+msrvtt.py
+---
 
+Defines MSRVTT dataset.
+"""
 import torch
 import torch.utils.data as data
 
 import src.utils.utility as _util
+
 
 class MSRVTTDataset(data.Dataset):
     def __init__(self, dataset, mode) -> None:
@@ -12,32 +16,10 @@ class MSRVTTDataset(data.Dataset):
 
         dataset_dir = _util.getDatasetByName(dataset, mode)
         self._features = _util.loadArray(dataset_dir, "frames")
-
-        msrvtt_dir = _util.getRawDatasetByName("MSRVTT", mode)
-        with open(os.path.join(msrvtt_dir, "annotations.json"), 'r') as f:
-            j = json.load(f)
-
-        splits = {}
-        for video in j["videos"]:
-            splits[video["video_id"]] = video["split"]
-
-        video_ids = []
-        captions = []
-        lengths = []
-        for sentence in j["sentences"]:
-            video_ids.append(sentence["video_id"])
-            captions.append(sentence["caption"])
-            lengths.append(len(sentence["caption"]))
-
-        vid2Idx = {video_id: idx for idx, video_id in enumerate(sorted(set(video_ids)))}
-        for video_id in video_ids:
-            assert splits[video_id] == mode, "{} should have split mode {} but has {}".format(video_id, splits[video_id], mode)
-            assert video_id in vid2Idx, "Video {} not found in internal map".format(video_id)
-
-        self._vid2Idx = vid2Idx
-        self._video_ids = video_ids
-        self._captions = captions
-        self._lengths = lengths
+        self._captions = _util.loadArray(dataset_dir, "captions")
+        self._lengths = _util.loadArray(dataset_dir, "cap_lens")
+        self._video_ids = _util.loadArray(dataset_dir, "video_ids")
+        self._vid2Idx = {video_id: idx for idx, video_id in enumerate(sorted(set(self._video_ids)))}
 
     def __getitem__(self, index):
         video_id = self._video_ids[index]
@@ -46,3 +28,5 @@ class MSRVTTDataset(data.Dataset):
 
     def __len__(self):
         return len(self._captions)
+
+MSRVTTDataset("MSRVTT", "train")
